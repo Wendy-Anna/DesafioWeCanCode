6f9b4d286178424adccbcfd23cd069894b3b4dfe
Wendy-Anna <wendyufpb@gmail.com>
2021-01-17 00:53:06 -0300

    Cards

--- service.js
+++ service.js
@@ -1,8 +1,35 @@
+Cards();
+
 function Get(Url) {
   var Httpreq = new XMLHttpRequest(); // a new request
   Httpreq.open("GET", Url, false);
   Httpreq.send();
   return Httpreq.responseText;
 }
-const json_obj = JSON.parse(Get("https://dry-cliffs-94979.herokuapp.com/"));
-console.log("this is the author name: " + json_obj[0].name);
+
+function getContainer() {
+  return document.getElementById("cards");
+}
+
+function Cards() {
+  const container = getContainer();
+  // const json_obj = JSON.parse(Get("https://dry-cliffs-94979.herokuapp.com/"), (key, value) => {
+
+  // });
+  const json_obj = JSON.parse(Get("https://dry-cliffs-94979.herokuapp.com/"));
+  for (let i in json_obj) {
+
+    container.innerHTML +=
+      '<div class="col-md-4">'
+      + ' <div class="card" style="width: 18rem;"> '
+      + '<img src="' + json_obj[i].photo + '" class="card-img-top" alt="..."> '
+      + ' <div class="card-body"> '
+      + ' <h5 class="card-title" >' + json_obj[i].property_type + '</h5> '
+      + ' <p class="card-text">R$ ' + json_obj[i].price + ' </p> '
+      + ' <a href="#" class="btn btn-primary">Conhecer</a> '
+      + ' </div> '
+      + ' </div> '
+      + '</div> '
+  }
+
+}
